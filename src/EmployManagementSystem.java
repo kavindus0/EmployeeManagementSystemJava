ae7e7aa10d27edf1d7f6935f0a449e0967b26b66
kavindus0 <kavindusachinthe@outlook.com>
2024-12-10 01:01:51 +0530

    comment removed

--- src/EmployManagementSystem.java
+++ src/EmployManagementSystem.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 
 
-/****************** Main Class ********************/
 class EmployManagementSystem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
