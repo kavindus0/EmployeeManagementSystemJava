73eabea2ab219e683588bbee63d51becf49672f8
kavindus0 <kavindusachinthe@outlook.com>
2024-12-09 20:37:59 +0530

    refactor

---
+++ src/CodeExit.java
@@ -0,0 +1,7 @@
+/****************** Exit Program ********************/
+class CodeExit {
+    public void out() {
+        System.out.println("\nThank you for using the Employee Management System!");
+        System.exit(0);
+    }
+}
