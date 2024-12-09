edd4907ed3330411205d930e5f9d7b9bdb8f3f51
kavindus0 <kavindusachinthe@outlook.com>
2024-12-09 20:38:23 +0530

    refactor

---
+++ src/MainMenu.java
@@ -0,0 +1,13 @@
+/****************** MENU OF EMS ********************/
+class MainMenu {
+    public void menu() {
+        System.out.println("\t\t*******************************************");
+        System.out.println("\t\t\t  EMPLOYEE MANAGEMENT SYSTEM");
+        System.out.println("\t\t*******************************************");
+        System.out.println("\nPress 1 : To Add an Employee Details");
+        System.out.println("Press 2 : To See an Employee Details");
+        System.out.println("Press 3 : To Remove an Employee");
+        System.out.println("Press 4 : To Update Employee Details");
+        System.out.println("Press 5 : To Exit the EMS Portal");
+    }
+}
