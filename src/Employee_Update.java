2212d7cb68f08b5a52a6796a6291d002b98b0105
kavindus0 <kavindusachinthe@outlook.com>
2024-12-09 20:38:15 +0530

    refactor

---
+++ src/Employee_Update.java
@@ -0,0 +1,27 @@
+/****************** Update Employee Details ********************/
+class Employee_Update {
+    public void updateFile(String id, String oldData, String newData) {
+        try {
+            File file = new File("employee" + id + ".html");
+            if (!file.exists()) {
+                System.out.println("Employee record not found.");
+                return;
+            }
+
+            Scanner sc = new Scanner(file);
+            StringBuilder fileContent = new StringBuilder();
+            while (sc.hasNextLine()) {
+                fileContent.append(sc.nextLine()).append("\n");
+            }
+
+            String updatedContent = fileContent.toString().replace(oldData, newData);
+            FileWriter writer = new FileWriter(file);
+            writer.write(updatedContent);
+            writer.close();
+
+            System.out.println("Employee record has been updated successfully!");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
