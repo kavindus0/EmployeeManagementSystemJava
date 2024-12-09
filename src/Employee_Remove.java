d4cacb34ea6836a80f8a2760de6c0cf88bdf84ca
kavindus0 <kavindusachinthe@outlook.com>
2024-12-09 20:38:08 +0530

    refactor

---
+++ src/Employee_Remove.java
@@ -0,0 +1,13 @@
+/****************** Remove Employee Details ********************/
+class Employee_Remove {
+    public void removeFile(String id) {
+        File file = new File("employee" + id + ".html");
+        if (file.exists()) {
+            if (file.delete()) {
+                System.out.println("\nEmployee has been removed successfully!");
+            }
+        } else {
+            System.out.println("\nEmployee does not exist!");
+        }
+    }
+}
