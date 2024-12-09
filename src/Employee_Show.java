a43a48b63a52649095e5ff7cf53cccef0f148cbc
kavindus0 <kavindusachinthe@outlook.com>
2024-12-09 20:38:12 +0530

    refactor

---
+++ src/Employee_Show.java
@@ -0,0 +1,14 @@
+/****************** Show Employee Details ********************/
+class Employee_Show {
+    public void viewFile(String id) throws Exception {
+        File file = new File("employee" + id + ".html");
+        if (file.exists()) {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } else {
+            System.out.println("Employee record not found.");
+        }
+    }
+}
