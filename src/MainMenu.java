e6e6a943e0d6889d42ea695d12d906e2551aa74d
kavindus0 <kavindusachinthe@outlook.com>
2024-12-11 21:02:32 +0530

    Intro re design

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,8 +1,20 @@
 class MainMenu {
     public void menu() {
-        System.out.println("\t\t*******************************************");
-        System.out.println("\t\t\t  EMPLOYEE MANAGEMENT SYSTEM");
-        System.out.println("\t\t*******************************************");
+
+        System.out.printf("                         .         .                        \n" +
+                "8 8888888888            ,8.       ,8.            d888888o.  \n" +
+                "8 8888                 ,888.     ,888.         .`8888:' `88.\n" +
+                "8 8888                .`8888.   .`8888.        8.`8888.   Y8\n" +
+                "8 8888               ,8.`8888. ,8.`8888.       `8.`8888.    \n" +
+                "8 888888888888      ,8'8.`8888,8^8.`8888.       `8.`8888.   \n" +
+                "8 8888             ,8' `8.`8888' `8.`8888.       `8.`8888.  \n" +
+                "8 8888            ,8'   `8.`88'   `8.`8888.       `8.`8888. \n" +
+                "8 8888           ,8'     `8.`'     `8.`8888.  8b   `8.`8888.\n" +
+                "8 8888          ,8'       `8        `8.`8888. `8b.  ;8.`8888\n" +
+                "8 888888888888 ,8'         `         `8.`8888. `Y8888P ,88P'");
+
+        System.out.println("===========================================================================================");
+        System.out.println("                          Welcome to Employee Management System                            ");
         System.out.println("\nPress 1 : To Add an Employee Details");
         System.out.println("Press 2 : To See an Employee Details");
         System.out.println("Press 3 : To Remove an Employee");
