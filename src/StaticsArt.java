f4562a1c25e625be7f94e27d05f9cc4bfc95efc3
kavindus0 <kavindusachinthe@outlook.com>
2024-12-11 21:44:22 +0530

    static file created

--- src/StaticsArt.java
+++ src/StaticsArt.java
@@ -1,4 +1,4 @@
-public class Arts {
+public class StaticsArt {
     public static void arts() {
 
 
@@ -16,5 +16,13 @@ public class Arts {
 
         System.out.println("\n\n==================================================================");
         System.out.println("              Welcome to Employee Management System                   ");
+    }
+
+    public static void SelectList() {
+        System.out.println("\nPress 1 : To Add an Employee Details");
+        System.out.println("Press 2 : To See an Employee Details");
+        System.out.println("Press 3 : To Remove an Employee");
+        System.out.println("Press 4 : To Update Employee Details");
+        System.out.println("Press 5 : To Exit the EMS Portal");
     }
 }
