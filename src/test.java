a364d7f1a8d724914dd89841508e24ca98cd6f7a
kavindus0 <kavindusachinthe@outlook.com>
2024-12-11 21:59:30 +0530

    implement for testing purpose

---
+++ src/test.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class test {
+    public static void main(String[] args) throws IOException {
+        openDirectory();
+    }
+
+
+    private static void openDirectory() throws IOException {
+        File directory = new File("/Users/kavindus/Projects/Sem02/JavaSem02/style.css");
+        Desktop.getDesktop().open(directory);
+
+
+    }
+}
