fc93f20974fb7f0bdbd90dbee61edc01af470724
kavindus0 <kavindusachinthe@outlook.com>
2024-12-11 22:07:32 +0530

    file view implemented

--- src/Employee_Show.java
+++ src/Employee_Show.java
@@ -1,13 +1,23 @@
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 /****************** Show Employee Details ********************/
 class Employee_Show {
+
+    private static void openDirectory(File file) throws IOException {
+      //  File directory = new File("/Users/kavindus/Projects/Sem02/JavaSem02/style.css");
+        Desktop.getDesktop().open(file);
+    }
+
     public void viewFile(String id) throws Exception {
         File file = new File("employee" + id + ".html");
+
         if (file.exists()) {
-            ///
-//                file code from test
+            Desktop.getDesktop().open(file);
+
+                         //file code from test
             Scanner sc = new Scanner(file);
             while (sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
@@ -17,3 +27,4 @@ class Employee_Show {
         }
     }
 }
+
