ff0a0505b8a7bb0bc25a616c0f2e8ff99be5e512
kavindus0 <kavindusachinthe@outlook.com>
2024-12-11 21:44:08 +0530

    reformatted

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,15 +1,8 @@
 class MainMenu {
     public void menu() {
-        Arts.arts();
-
-        SelectList();
+        StaticsArt.arts();
+      //  StaticsArt.SelectList();
     }
 
-    public static void SelectList() {
-        System.out.println("\nPress 1 : To Add an Employee Details");
-        System.out.println("Press 2 : To See an Employee Details");
-        System.out.println("Press 3 : To Remove an Employee");
-        System.out.println("Press 4 : To Update Employee Details");
-        System.out.println("Press 5 : To Exit the EMS Portal");
-    }
+
 }
