1043d22d792b31b447be1f81cd4efc6697fd507c
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-22 01:22:17 +0530

    salary updated

---
+++ src/SalaryMaths.java
@@ -0,0 +1,13 @@
+public class SalaryMaths implements Calculations{
+
+    @Override
+    public int daysInMonth(int month) {
+        if (month == 2) {
+            return 28;
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            return 30;
+        } else {
+            return 31;
+        }
+    }
+}
