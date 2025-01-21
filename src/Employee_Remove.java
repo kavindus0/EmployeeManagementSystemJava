706d36700b0cab961f53af0633c66c9cf1f81800
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-22 00:00:56 +0530

    file location added

--- src/Employee_Remove.java
+++ src/Employee_Remove.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 class Employee_Remove {
     public void removeFile(String id) {
-        File file = new File("employee" + id + ".html");
+        File file = new File("./userdata/employee" + id + ".html");
         if (file.exists()) {
             if (file.delete()) {
                 System.out.println("\nEmployee has been removed successfully!");
