45a47acde9f672850eeacb19decca5ed4c0d78e5
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-22 13:31:24 +0530

    emergency contact added

---
+++ src/EmergencyContactAddPage.java
@@ -0,0 +1,6 @@
+public class EmergencyContactAddPage extends EmergencyContact{
+    public EmergencyContactAddPage(String name, String contactNo, String email, String address) {
+        super(name, contactNo, email, address);
+    }
+
+}
