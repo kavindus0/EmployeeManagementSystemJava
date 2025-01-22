38a81d408f095f36d79c5a56c1a138f3e81dfef2
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-22 14:45:51 +0530

    employee feild modified

--- src/EmployDetail.java
+++ src/EmployDetail.java
@@ -2,49 +2,71 @@ import java.io.IOException;
 import java.util.Scanner;
 class EmployDetail extends Person{
    private String name;
-   private String father_name;
    private String email;
    private String position;
+   private String Adress;
    private String employ_id;
-    private double employ_salary;
+   private double employ_salary;
    private int employ_contact;
+   private String emergencyContact_name;
+   private String emergencyContact_contact;
+   private String emergencyContact_adress;
+   private String emergencyContact_email;
 
-    public EmployDetail(String employ_id, double employ_salary,String name) {
-        super(name);
-        this.employ_id = employ_id;
-        this.employ_salary = employ_salary;
+
+    public EmployDetail(String name, String contactNo, String email, String address, String emergencyContact_adress, String emergencyContact_contact, String emergencyContact_email, String emergencyContact_name) {
+        super(name, contactNo, email, address);
+        this.emergencyContact_adress = emergencyContact_adress;
+        this.emergencyContact_contact = emergencyContact_contact;
+        this.emergencyContact_email = emergencyContact_email;
+        this.emergencyContact_name = emergencyContact_name;
+    }
+
+    public EmployDetail() {
+        super();
+         this.emergencyContact_adress = emergencyContact_adress;
+        this.emergencyContact_contact = emergencyContact_contact;
+        this.emergencyContact_email = emergencyContact_email;
+        this.emergencyContact_name = emergencyContact_name;
     }
 
     @Override
     public String toString() {
         return "EmployDetail{" +
-                "email='" + email + '\'' +
+                "Adress='" + Adress + '\'' +
                 ", name='" + name + '\'' +
-                ", father_name='" + father_name + '\'' +
+                ", email='" + email + '\'' +
                 ", position='" + position + '\'' +
                 ", employ_id='" + employ_id + '\'' +
                 ", employ_salary=" + employ_salary +
                 ", employ_contact=" + employ_contact +
-                "} ";
+                ", emergencyContact_name='" + emergencyContact_name + '\'' +
+                ", emergencyContact_contact='" + emergencyContact_contact + '\'' +
+                ", emergencyContact_adress='" + emergencyContact_adress + '\'' +
+                ", emergencyContact_email='" + emergencyContact_email + '\'' +
+                "} " + super.toString();
     }
 
-    public EmployDetail(String email, int employ_contact, String employ_id, double employ_salary, String father_name, String name, String position) {
-        super(name);
+    public EmployDetail(String s, int employContact, String name, Object address, String adress, String email, String emergencyContact_adress, String emergencyContact_contact, String emergencyContact_email, String emergencyContact_name, int employ_contact, String employ_id, double employ_salary, String name1, String position) {
+        super(s, employContact, name, address);
+        Adress = adress;
         this.email = email;
+        this.emergencyContact_adress = emergencyContact_adress;
+        this.emergencyContact_contact = emergencyContact_contact;
+        this.emergencyContact_email = emergencyContact_email;
+        this.emergencyContact_name = emergencyContact_name;
         this.employ_contact = employ_contact;
         this.employ_id = employ_id;
         this.employ_salary = employ_salary;
-        this.father_name = father_name;
-        this.name = name;
+        this.name = name1;
         this.position = position;
     }
 
-    public EmployDetail() {
-        super("");
+    public EmployDetail(String name, String contactNo, String email, String address) {
+        super(name, contactNo, email, address);
     }
 
-
-   public double getSalary() {
+    public double getSalary() {
         return employ_salary;
     }
     public String getEmail() {
@@ -59,9 +81,7 @@ class EmployDetail extends Person{
         return employ_id;
     }
 
-    public String getFather_name() {
-        return father_name;
-    }
+
 
     public String getName() {
         return name;
@@ -92,11 +112,32 @@ class EmployDetail extends Person{
         }
 
         // Get Father Name
-        System.out.print("Enter " + name + "'s Father Name -: ");
-        father_name = sc.nextLine().trim();
-        while (father_name.isEmpty()) {
-            System.out.print("Father's name cannot be empty. Enter " + name + "'s Father Name -: ");
-            father_name = sc.nextLine().trim();
+        System.out.print("Enter " + name + "'s Emergency Contact's Name -: ");
+        emergencyContact_name = sc.nextLine().trim();
+        while (emergencyContact_name.isEmpty()) {
+            System.out.print("Emergency Contact's name cannot be empty. Enter " + name + "Name -: ");
+            emergencyContact_name = sc.nextLine().trim();
+        }
+
+          System.out.print("Enter " + name + "'s Emergency Contact -: ");
+        emergencyContact_contact = sc.nextLine().trim();
+        while (emergencyContact_contact.isEmpty()) {
+            System.out.print("Emergency Contact cannot be empty. Enter " + name + "Contact No -: ");
+            emergencyContact_contact = sc.nextLine().trim();
+        }
+
+          System.out.print("Enter " + name + "'s Emergency Contact Adress-: ");
+        emergencyContact_adress = sc.nextLine().trim();
+        while (emergencyContact_adress.isEmpty()) {
+            System.out.print("Emergency Contact cannot be empty. Enter " + name + "Contact No -: ");
+            emergencyContact_adress = sc.nextLine().trim();
+        }
+
+         System.out.print("Enter " + name + "'s Emergency Contact email-: ");
+        emergencyContact_email = sc.nextLine().trim();
+        while (emergencyContact_email.isEmpty()) {
+            System.out.print("Emergency Contact cannot be empty. Enter " + name + "Contact No -: ");
+            emergencyContact_email = sc.nextLine().trim();
         }
 
         // Get Employee ID
