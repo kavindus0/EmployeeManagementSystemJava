bd2d09a148fd808a3b92917d527c19b1a317dd79
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-22 14:45:21 +0530

    emergency contact constructors created

--- src/EmergencyContact.java
+++ src/EmergencyContact.java
@@ -1,5 +1,9 @@
 public class EmergencyContact extends Person{
     public EmergencyContact(String name, String contactNo, String email, String address) {
-        super(name);
+        super(name, contactNo, email, address);
+    }
+
+    public EmergencyContact(String name, int employContact, String email, Object address) {
+        super(name, employContact, email, address);
     }
 }
