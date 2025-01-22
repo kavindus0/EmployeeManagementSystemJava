69e0461c9fd26b875698a18c3f884ab63d9f67bf
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-22 13:30:49 +0530

    user implemented

--- src/User.java
+++ src/User.java
@@ -1,17 +1,26 @@
-public class User {
-    private String username;
+public class User extends Person {
+    private String name;
     private String password;
 
-    public User(String username, String password) {
-        this.username = username;
+    public User(String name, String password) {
+        super(name);
+        this.name = name;
         this.password = password;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                "} ";
     }
 }
